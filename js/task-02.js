bef93b598ff97f46f8d9f9c3ecbe0704a5722a7c
RobertDoms <robertdoms90@gmail.com>
2022-09-29 21:38:35 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsList = document.querySelector(`ul`);
+ingredients.textContent = "Ingredients:";
+
+const items = ingredients.map(ingredient => {
+  const item = document.createElement(`li`);
+  item.textContent = ingredient;
+  item.classList.add("item");
+  return item;
+});
+
+console.log(items);
+ingredientsList.append(...items);
