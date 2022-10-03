8f7d24ef1eac81b21e9cb58503083eaccd4d0e4a
RobertDoms <robertdoms90@gmail.com>
2022-10-03 12:32:50 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  input: document.querySelector('input'),
+  createBtn: document.querySelector('[data-create]'),
+  destroyBtn: document.querySelector('[data-destroy]'),
+  boxes: document.querySelector('#boxes'),
+  controls: document.querySelector('#controls'),
+};
+
+refs.controls.classList.add('item-tt');
