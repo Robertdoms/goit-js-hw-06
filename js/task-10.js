816200ddcc280ee5bbce0145c6e0cfad8266d7bb
RobertDoms <robertdoms90@gmail.com>
2022-10-03 20:38:50 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -10,7 +10,28 @@ const refs = {
   controls: document.querySelector("#controls"),
 };
 
-refs.controls.classList.add("item-tt");
+// refs.controls.classList.add("item-tt");
 
 refs.createBtn.addEventListener("click", createPaletteItems);
 refs.destroyBtn.addEventListener("click", onDestroyBtnClick);
+
+function createPaletteItems() {
+  const items = [];
+  let amount = Number(refs.input.value);
+  for (let i = 0; i < amount; i += 1) {
+    const color = getRandomHexColor();
+    const item = document.createElement("div");
+    item.style.width = 30 + i * 10 + "px";
+    item.style.height = 30 + i * 10 + "px";
+    item.style.margin = 10 + "px";
+    item.style.backgroundColor = color;
+    item.style.borderRadius = 4 + "px";
+    item.classList.add("item-tt");
+    items.push(item);
+  }
+  refs.boxes.append(...items);
+}
+
+function onDestroyBtnClick() {
+  refs.boxes.innerHTML = "";
+}
